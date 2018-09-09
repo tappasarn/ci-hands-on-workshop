153687b953fae707b19e5f1efbdc3080eb4ebb5e
tappasarn <time723@gmail.com>
2018-09-09 15:45:55 +0700

    start step

--- src/main/java/utils/CurrencyFormatter.java
+++ src/main/java/utils/CurrencyFormatter.java
@@ -4,35 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class CurrencyFormatter {
-
-    public String formatWithDecimalFormat(String amount) {
+    public String format(String amount) {
         Double dAmount = Double.parseDouble(amount);
         DecimalFormat d = new DecimalFormat("#,###.##");
         return d.format(dAmount);
-    }
-
-    public String format(String amount) {
-        char[] amountArr = amount.toCharArray();
-        ArrayList<Character> convertedList = new ArrayList<>();
-
-        int y = 0;
-        for(int i = amountArr.length-1; i >= 0; i--){
-            if(y==3) {
-                convertedList.add(',');
-                y=0;
-            }
-            convertedList.add(amountArr[i]);
-            y++;
-        }
-
-        Collections.reverse(convertedList);
-
-        StringBuilder builder = new StringBuilder(convertedList.size());
-        for(Character ch: convertedList)
-        {
-            builder.append(ch);
-        }
-
-        return builder.toString();
     }
 }
--- src/test/java/utils/CurrencyFormatterTests.java
+++ src/test/java/utils/CurrencyFormatterTests.java
@@ -1,38 +1,4 @@
 package utils;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class CurrencyFormatterTests {
-    @Test public void ShouldReturnCorrectAmount() {
-        CurrencyFormatter c = new CurrencyFormatter();
 
-        String result = c.format("1234");
-
-        assertEquals("1,234", result);
-    }
-
-    @Test public void ShouldReturnCorrectAmountWithBiggerNumber() {
-        CurrencyFormatter c = new CurrencyFormatter();
-
-        String result = c.format("123456789");
-
-        assertEquals("123,456,789", result);
-    }
-
-    @Test public void UseDecimalFormatTest() {
-        CurrencyFormatter c = new CurrencyFormatter();
-
-        String result = c.formatWithDecimalFormat("1234");
-
-        assertEquals("1,234", result);
-    }
-
-    @Test public void UseDecimalFormatWithBiggerNumberTest() {
-        CurrencyFormatter c = new CurrencyFormatter();
-
-        String result = c.formatWithDecimalFormat("123456789");
-
-        assertEquals("123,456,789", result);
-    }
 }
