d619b8f0d1c4b1ecbdac94dccb60f82c090e5ac7
Nutchanon Anurakboonying <nutchanon.anurakboonying@agoda.com>
2018-09-09 15:25:46 +0700

    Update unit test

--- src/test/java/utils/CurrencyFormatterTests.java
+++ src/test/java/utils/CurrencyFormatterTests.java
@@ -20,11 +20,19 @@ public class CurrencyFormatterTests {
         assertEquals("123,456,789", result);
     }
 
-    @Test public void UseDecimalFormat() {
+    @Test public void UseDecimalFormatTest() {
         CurrencyFormatter c = new CurrencyFormatter();
 
         String result = c.formatWithDecimalFormat("1234");
 
         assertEquals("1,234", result);
+    }
+
+    @Test public void UseDecimalFormatWithBiggerNumberTest() {
+        CurrencyFormatter c = new CurrencyFormatter();
+
+        String result = c.formatWithDecimalFormat("123456789");
+
+        assertEquals("123,456,789", result);
     }
 }
