a2290c7c84db6d662713bf8a38d750d7e8488219
tappasarn <time723@gmail.com>
2018-09-23 12:23:14 +0700

    add incorrect code and unit test

--- src/main/java/utils/CurrencyFormatter.java
+++ src/main/java/utils/CurrencyFormatter.java
@@ -1,11 +1,29 @@
 package utils;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CurrencyFormatter {
     public String format(String amount) {
-        Double dAmount = Double.parseDouble(amount);
-        DecimalFormat d = new DecimalFormat("#,###.##");
-        return d.format(dAmount);
+        char[] amountArr = amount.toCharArray();
+        ArrayList<Character> convertedList = new ArrayList<>();
+        
+        for(int i = amountArr.length-1; i >= 0; i--){
+            if(i % 3 == 0 && i != amountArr.length-1) {
+                convertedList.add(',');
+            }
+            convertedList.add(amountArr[i]);
+        }
+        
+        Collections.reverse(convertedList);
+        
+        StringBuilder builder = new StringBuilder(convertedList.size());
+        for(Character ch: convertedList)
+        {
+            builder.append(ch);
+        }
+        
+        return builder.toString();
     }
 }
