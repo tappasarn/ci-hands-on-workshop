4c48224c66c43d9dbdcc4bea152b696938de14d8
robroooh <robroo.pc@gmail.com>
2018-10-28 20:20:15 +0200

    fail me

--- src/test/java/utils/CurrencyFormatterTests.java
+++ src/test/java/utils/CurrencyFormatterTests.java
@@ -1,5 +1,30 @@
 package utils;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CurrencyFormatterTests {
 
+  @Test public void ShouldReturnFormattedNumberWithSingleComma() {
+      CurrencyFormatter c = new CurrencyFormatter();
+
+      String result = c.format("1234");
+
+      assertEquals("1,234", result);
+  }
+  @Test public void ShouldReturnFormattedNumberWithMultipleCommas() {
+      CurrencyFormatter c = new CurrencyFormatter();
+
+      String result = c.format("1234567");
+
+      assertEquals("1,234,567", result);
+  }
+  @Test public void ShouldReturnFormattedNumberWithNoComma() {
+      CurrencyFormatter c = new CurrencyFormatter();
+
+      String result = c.format("123");
+
+      assertEquals("123", result);
+  }
+
 }
+
